668c6b8fead139d4d10e03c39fbfaa78a85fe8be
ronald curitao <dna.ronron@gmail.com>
2012-11-27 04:18:13 +0800

    TCP client chat app

---
+++ ClientChatApplication/Notifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClientChatApplication
+{
+    public partial class Notifier : Form
+    {
+        public Notifier()
+        {
+            InitializeComponent();
+        }
+    }
+}
