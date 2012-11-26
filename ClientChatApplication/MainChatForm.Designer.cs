4455639d7c64285dd7f64c04a6be141eef8ef577
ron123ald <ron123ald@gmail.com>
2012-11-26 22:43:10 +0800

    1st commit to TCP Chat Client Application

---
+++ ClientChatApplication/MainChatForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ClientChatApplication
+{
+    partial class MainChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.JoinButton = new System.Windows.Forms.Button();
+            this.UsernameBox = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(521, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Activity";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.UsernameBox);
+            this.groupBox2.Controls.Add(this.JoinButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(521, 59);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Information";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(545, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // JoinButton
+            // 
+            this.JoinButton.Location = new System.Drawing.Point(364, 24);
+            this.JoinButton.Name = "JoinButton";
+            this.JoinButton.Size = new System.Drawing.Size(75, 23);
+            this.JoinButton.TabIndex = 0;
+            this.JoinButton.Text = "Join";
+            this.JoinButton.UseVisualStyleBackColor = true;
+            this.JoinButton.Click += new System.EventHandler(this.JoinButton_Click);
+            // 
+            // UsernameBox
+            // 
+            this.UsernameBox.Location = new System.Drawing.Point(89, 26);
+            this.UsernameBox.Name = "UsernameBox";
+            this.UsernameBox.Size = new System.Drawing.Size(259, 20);
+            this.UsernameBox.TabIndex = 1;
+            // 
+            // MainChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 420);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainChatForm";
+            this.Text = "Chat Client Application";
+            this.Load += new System.EventHandler(this.MainChatForm_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.Button JoinButton;
+        private System.Windows.Forms.TextBox UsernameBox;
+    }
+}
+
---
+++ ClientChatApplication/MainChatForm.cs
@@ -0,0 +1,53 @@
+﻿namespace ClientChatApplication
+{
+    using System;
+    using System.Windows.Forms;
+    using ChatClientLibrary;
+    using ChatClientLibrary.Chat.Factory;
+
+    public partial class MainChatForm : Form
+    {
+        private IChatClient chatClient = default(IChatClient);
+
+        public MainChatForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainChatForm_Load(object sender, EventArgs e)
+        {
+            ChatFactory factory = new ChatFactory();
+            this.chatClient = factory.CreateConnection();
+            this.chatClient.LogEvent += chatClient_LogEvent;
+            this.chatClient.NewMessageEvent += chatClient_NewMessageEvent;
+            this.chatClient.NewUserEvent += chatClient_NewUserEvent;
+            this.chatClient.UserLeaveEvent += chatClient_UserLeaveEvent;
+        }
+
+        #region Chat Client EventHandlers
+        
+        private void chatClient_UserLeaveEvent(object sender, ChatClientLibrary.Chat.Events.LeaveEventArgs e)
+        {
+        }
+
+        private void chatClient_NewUserEvent(object sender, ChatClientLibrary.Chat.Events.NewUserEventArgs e)
+        {
+        }
+
+        private void chatClient_NewMessageEvent(object sender, ChatClientLibrary.Chat.Events.NewMessageEventArgs e)
+        {
+        }
+
+        private void chatClient_LogEvent(object sender, ChatClientLibrary.Chat.Events.LogEventArgs e)
+        {
+        } 
+
+        #endregion
+
+        private void JoinButton_Click(object sender, EventArgs e)
+        {
+            /// Join Action
+            /// and SendAction to chatClient
+        }
+    }
+}
